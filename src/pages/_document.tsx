e7d524d86a4248f5b114f43d578f714a259240ed
Xander Low <alexanderjameslow@gmail.com>
2022-06-21 23:12:52 -0700

    fixed: crossOrigin

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,11 +23,6 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin
-          />
-          <link
             href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,200;0,400;0,600;0,700;1,200;1,400;1,600;1,700&display=swap"
             rel="stylesheet"
           />

929af6d72abfebeb5c7d08b8099c8f6c2ac8aa60
Xander Low <alexanderjameslow@gmail.com>
2022-06-29 09:31:16 -0700

    chore: continue through essay

--- src/pages/sketches/learning-more-flow-fields.tsx
+++ src/pages/sketches/learning-more-flow-fields.tsx
@@ -2,7 +2,9 @@ import SketchWrapper from 'components/SketchWrapper'
 import { NextPage } from 'next'
 import { Sketch } from 'react-p5-wrapper'
 
-const sketch: Sketch = p5 => {
+const sketch: Sketch = async p5 => {
+  const Vector = await import('p5').then(mod => mod.default.Vector)
+
   const left: number = p5.windowWidth * -0.5
   const right: number = p5.windowWidth * 1.5
   const top: number = p5.windowHeight * -0.5
@@ -17,11 +19,23 @@ const sketch: Sketch = p5 => {
 
   p5.setup = () => {
     p5.createCanvas(1000, 1000)
+    p5.background(0)
+    p5.stroke(255)
+    p5.strokeWeight(1)
+
     grid.forEach((cell, col) => {
       cell.forEach((_, row) => {
         const angle: number = (row / numRows) * Math.PI
 
         grid[col][row] = angle
+
+        const vector = Vector.fromAngle(angle)
+        const { x, y } = vector
+
+        p5.beginShape()
+        p5.curveVertex(x, y)
+        p5.curveVertex(row * res, col * res)
+        p5.endShape()
       })
     })
   }

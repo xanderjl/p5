354d2d588e7ee1214f800abd1570d87b059ea4fb
Xander Low <alexanderjameslow@gmail.com>
2022-06-29 21:41:49 -0700

    lodash

--- src/pages/sketches/circles.tsx
+++ src/pages/sketches/circles.tsx
@@ -1,4 +1,5 @@
 import SketchWrapper from 'components/SketchWrapper'
+import _ from 'lodash'
 import { NextPage } from 'next'
 import { Sketch } from 'react-p5-wrapper'
 
@@ -9,15 +10,16 @@ interface Circle {
 }
 
 const sketch: Sketch = p5 => {
-  const circles: Circle[] = []
+  let circles: Circle[] = []
   const maxR: number = 90
+  const background = () => p5.background(260, 200, 20)
 
   p5.setup = () => {
     p5.createCanvas(p5.windowWidth, p5.windowHeight)
     p5.colorMode(p5.HSB)
-    p5.background(260, 200, 20)
+    background()
 
-    Array.from({ length: p5.windowWidth * 2 }).forEach((_, i) => {
+    Array.from({ length: p5.windowWidth * 2 }).forEach(() => {
       const circle: Circle = {
         x: p5.random(p5.width),
         y: p5.random(p5.height),
@@ -49,6 +51,44 @@ const sketch: Sketch = p5 => {
       p5.ellipse(x, y, r * 2, r * 2)
     })
   }
+
+  p5.windowResized = _.debounce(() => {
+    p5.resizeCanvas(p5.windowWidth, p5.windowHeight)
+    background()
+    circles = []
+
+    Array.from({ length: p5.windowWidth * 2 }).forEach(() => {
+      const circle: Circle = {
+        x: p5.random(p5.width),
+        y: p5.random(p5.height),
+        r: p5.random(12, 128),
+      }
+      const overlapping = circles.find(other => {
+        const radii = circle.r + other.r
+        const d = p5.dist(circle.x, circle.y, other.x, other.y)
+        if (d < radii + radii * 0.2) {
+          return true
+        }
+      })
+
+      !overlapping && circle.r < maxR && circles.push(circle)
+    })
+
+    circles.forEach(({ x, y, r }) => {
+      const rand: number = Math.random()
+      const h =
+        rand < 0.25
+          ? p5.random(10, 40)
+          : rand > 0.25 && rand < 0.5
+          ? p5.random(120, 200)
+          : rand > 0.5 && rand < 0.75
+          ? p5.random(60, 100)
+          : p5.random(280, 300)
+      p5.fill(h, p5.random(20, 40), p5.random(100, 150))
+      p5.noStroke()
+      p5.ellipse(x, y, r * 2, r * 2)
+    })
+  }, 100)
 }
 
 const Circles: NextPage = () => <SketchWrapper sketch={sketch} />

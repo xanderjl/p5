26257edd26dfbab1bc09efb8cf79c9e49f476399
Xander Low <alexanderjameslow@gmail.com>
2022-06-30 23:46:29 -0700

    added: file save functionality

--- src/components/SketchWrapper.tsx
+++ src/components/SketchWrapper.tsx
@@ -1,17 +1,20 @@
 import dynamic from 'next/dynamic'
-import { FC, useEffect, useState } from 'react'
+import { Dispatch, FC, useEffect, useState } from 'react'
 import type { P5WrapperProps, Sketch } from 'react-p5-wrapper'
 const ReactP5Wrapper = dynamic<P5WrapperProps>(
   () => import('react-p5-wrapper').then(mod => mod.ReactP5Wrapper),
   { ssr: false }
 )
+import { format } from 'date-fns'
 
 export interface SketchWrapperProps {
   sketch: Sketch
+  suffix?: string
 }
 
-const SketchWrapper: FC<SketchWrapperProps> = ({ sketch }) => {
+const SketchWrapper: FC<SketchWrapperProps> = ({ sketch, suffix }) => {
   const [os, setOs] = useState<string>('')
+
   useEffect(() => {
     if (typeof window !== 'undefined') {
       const osString = navigator.userAgent.includes('Mac') ? 'mac' : ''
@@ -20,16 +23,21 @@ const SketchWrapper: FC<SketchWrapperProps> = ({ sketch }) => {
   }, [os])
 
   const doTheOtherStuff: Sketch = p5 => {
+    const fileName = [
+      format(new Date(), 'yyyy.MM.dd-kk.mm.ss'),
+      suffix ? suffix : '',
+    ].join()
+
     p5.keyPressed = (e: KeyboardEvent) => {
       if (os === 'mac') {
         if (e.key === 's' && e.metaKey) {
           e.preventDefault()
-          p5.saveCanvas('bimbim', 'png')
+          p5.saveCanvas(fileName, 'png')
         }
       } else {
         if (e.key === 's' && e.ctrlKey) {
           e.preventDefault()
-          p5.saveCanvas('bimbim', 'png')
+          p5.saveCanvas(fileName, 'png')
         }
       }
     }

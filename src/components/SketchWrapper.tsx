5ddf3d087e2f94cc4c0b88c5e4ab0246d3bb487b
Xander Low <alexanderjameslow@gmail.com>
2022-06-30 22:52:53 -0700

    added: keypress

--- src/components/SketchWrapper.tsx
+++ src/components/SketchWrapper.tsx
@@ -1,5 +1,5 @@
 import dynamic from 'next/dynamic'
-import type { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import type { P5WrapperProps, Sketch } from 'react-p5-wrapper'
 const ReactP5Wrapper = dynamic<P5WrapperProps>(
   () => import('react-p5-wrapper').then(mod => mod.ReactP5Wrapper),
@@ -10,8 +10,33 @@ export interface SketchWrapperProps {
   sketch: Sketch
 }
 
-const SketchWrapper: FC<SketchWrapperProps> = ({ sketch }) => (
-  <ReactP5Wrapper sketch={sketch} />
-)
+const SketchWrapper: FC<SketchWrapperProps> = ({ sketch }) => {
+  const [os, setOs] = useState<string>('')
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const osString = navigator.userAgent.includes('Mac') ? 'mac' : ''
+      setOs(osString)
+    }
+  }, [os])
+
+  const doTheOtherStuff: Sketch = p5 => {
+    p5.keyPressed = (e: KeyboardEvent) => {
+      if (os === 'mac') {
+        if (e.key === 's' && e.metaKey) {
+          e.preventDefault()
+          p5.saveCanvas('bimbim', 'png')
+        }
+      }
+
+      if (e.key === 's' && e.ctrlKey) {
+        e.preventDefault()
+        p5.saveCanvas('bimbim', 'png')
+      }
+    }
+    return sketch(p5)
+  }
+
+  return <ReactP5Wrapper sketch={doTheOtherStuff} />
+}
 
 export default SketchWrapper

9b107509d3e1394266ab284779acbaa56149ec8f
Xander Low <alexanderjameslow@gmail.com>
2022-07-06 01:51:42 -0700

    fix this bullshit

---
+++ _templates/one-to-one/new/index.ejs.t
@@ -0,0 +1,27 @@
+---
+to: src/pages/sketches/<%= name %>.tsx
+---
+import SketchWrapper from 'components/SketchWrapper'
+import { NextPage } from 'next'
+import { Sketch } from 'react-p5-wrapper'
+
+const width: number = 2048
+const height: number = 2048
+const dimensions: number[] = [width, height]
+const padding: number[] = [40]
+const background: number[] = [255]
+
+const sketch: Sketch = p5 => {
+  p5.draw = () => {}
+}
+
+const <%= h.changeCase.pascal(name) %>: NextPage = () => (
+  <SketchWrapper
+    sketch={sketch}
+    dimensions={dimensions}
+    padding={padding}
+    background={background}
+  />
+)
+
+export default <%= h.changeCase.pascal(name) %>
---
+++ src/pages/sketches/triangle-shifting.tsx
@@ -0,0 +1,50 @@
+import SketchWrapper from 'components/SketchWrapper'
+import { NextPage } from 'next'
+import { Sketch } from 'react-p5-wrapper'
+import createGrid from 'util/createGrid'
+import { redGrid } from 'util/shapes'
+
+const width: number = 2048
+const height: number = 2048
+const dimensions: number[] = [width, height]
+const padding: number[] = [40]
+const background: number[] = [0]
+const count: number = 9
+const points = createGrid(count)
+const vertices: number[][] = [
+  [0, 0],
+  [0, count],
+  [count, count],
+]
+
+const sketch: Sketch = p5 => {
+  p5.draw = () => {
+    const scale: number = p5.width / 8
+    p5.background(0)
+    p5.frameRate(1)
+
+    p5.fill(255, 251, 245)
+    p5.beginShape()
+    vertices.forEach((ver, i) => {
+      if (ver[0] === vertices.length && ver[1] !== vertices.length) {
+        ver[1] = ver[1] - 2
+      }
+
+      p5.vertex(ver[0] * scale, ver[1] * scale)
+    })
+    p5.endShape()
+
+    redGrid(p5, points, scale)
+  }
+}
+
+const TriangleShifting: NextPage = () => (
+  <SketchWrapper
+    sketch={sketch}
+    dimensions={dimensions}
+    padding={padding}
+    background={background}
+  />
+)
+
+export default TriangleShifting

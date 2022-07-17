16b57b18c4127f53d1ef47cf1a4e3ecf90e8f247
Xander Low <alexanderjameslow@gmail.com>
2022-07-16 23:17:56 -0700

    find solve for loading plugins

--- src/pages/sketches/distribution.tsx
+++ src/pages/sketches/distribution.tsx
@@ -1,5 +1,6 @@
 import SketchWrapper from 'components/SketchWrapper'
 import { NextPage } from 'next'
+import { PointerEvent } from 'react'
 import { Sketch } from 'react-p5-wrapper'
 import { ColorValue } from 'types/CustomP5'
 import createGrid from 'util/createGrid'
@@ -10,19 +11,39 @@ const dimensions: number[] = [width, height]
 const padding: number[] = [40]
 const background: ColorValue = [255, 253, 252]
 let seed: number = 0
-const count: number = 40
-const grid = createGrid(count)
+const count: number = 100
 
-const sketch: Sketch = p5 => {
+const sketch: Sketch = async p5 => {
   p5.draw = () => {
+    p5.noLoop()
+    p5.background(background)
+    let iter: number = 0
+    const grid = createGrid(count).filter((_, i) => {
+      iter += 0.1
+      return (
+        p5.noise(seed, i * iter) >
+        p5.map(p5.noise(iter, iter), 0, iter, 0, 1) + 0.5
+      )
+    })
     const margin: number = Math.floor(p5.width / 10)
 
     grid.forEach(([u, v]) => {
       const x = p5.lerp(margin, p5.width - margin, u)
       const y = p5.lerp(margin, p5.height - margin, v)
 
-      p5.point(x, y)
+      p5.fill(0)
+      p5.rectMode(p5.CENTER)
+      p5.rect(x, y, (p5.width - margin * 2) / count)
     })
+
+    p5.mouseClicked = (e: PointerEvent) => {
+      if (e.shiftKey) {
+        seed -= 1
+      } else {
+        seed += 1
+      }
+      p5.draw()
+    }
   }
 }
 
@@ -32,6 +53,7 @@ const Distribution: NextPage = () => (
     dimensions={dimensions}
     padding={padding}
     background={background}
+    seed={seed}
     suffix={seed}
   />
 )

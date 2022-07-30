2696c5ccbf9d8cf8979349e29384871a0c6d5029
Xander Low <alexanderjameslow@gmail.com>
2022-07-30 16:49:33 -0700

    new sketches

---
+++ src/pages/sketches/keep-it-wavy.tsx
@@ -0,0 +1,110 @@
+import SketchWrapper from 'components/SketchWrapper'
+import { NextPage } from 'next'
+import { ColorValue, Draw, MouseClicked, Setup } from 'types/CustomP5'
+
+const width: number = 1000
+const height: number = 1000
+const dimensions: number[] = [width, height]
+const padding: number[] = [40]
+const background: ColorValue = [255, 253, 252]
+
+let xLeft: number
+let xRight: number
+let yTop: number
+let yBottom: number
+let resolution: number
+let numColumns: number
+let numRows: number
+let grid: number[][]
+let angle: number
+let seed: number = 0
+
+const setup: Setup = p5 => {
+  xLeft = p5.width * -0.5
+  xRight = p5.width * 1.5
+  yTop = p5.height * -0.5
+  yBottom = p5.height * 1.5
+  resolution = p5.width * 0.01
+  numColumns = (xRight - xLeft) / resolution
+  numRows = (yBottom - yTop) / resolution
+}
+
+const draw: Draw = p5 => {
+  grid = Array.from({ length: numColumns }, () =>
+    Array.from({ length: numRows })
+  )
+  p5.noiseSeed(seed)
+  p5.randomSeed(seed)
+  p5.noLoop()
+  p5.background(background)
+
+  // show flow field
+  grid = grid.map((_, x) => {
+    return _.map((_, y) => {
+      const xScaled = x * 0.005
+      const yScaled = y * 0.005
+      const noiseVal = p5.noise(xScaled, yScaled)
+      angle = p5.map(noiseVal, 0, 1, 0, p5.TWO_PI)
+      const vec = p5.constructor.Vector.fromAngle(angle)
+      vec.setMag(0.25)
+
+      p5.stroke(0)
+      p5.strokeWeight(1)
+      p5.push()
+      p5.translate(x * resolution, y * resolution)
+      p5.rotate(vec.heading())
+      p5.ellipse(0, 0, 2)
+      p5.line(0, 0, resolution, 0)
+      p5.pop()
+
+      return angle
+    })
+  })
+
+  let xStart = p5.map(p5.noise(seed, seed), 0, 1, 0, numColumns)
+  let yStart = p5.map(p5.noise(seed, seed), 0, 1, 0, numRows)
+  let stepLength = 4
+  // draw curve
+
+  p5.stroke('red')
+  p5.strokeWeight(3)
+  p5.noFill()
+  p5.beginShape()
+  Array.from({ length: 100 }).forEach(() => {
+    p5.vertex(xStart, yStart)
+    let xOff = xStart - xLeft
+    let yOff = yStart - yTop
+    let colIndex = Math.floor(xOff / resolution)
+    let rowIndex = Math.floor(yOff / resolution)
+    angle = grid[colIndex][rowIndex]
+    let xStep = stepLength * Math.cos(angle)
+    let yStep = stepLength * Math.sin(angle)
+
+    xStart = xStart + xStep
+    yStart = yStart + yStep
+  })
+  p5.endShape()
+}
+
+const mouseClicked: MouseClicked = (p5, e) => {
+  if (e.shiftKey) {
+    seed--
+  } else {
+    seed++
+  }
+  p5.loop()
+  p5.noLoop()
+}
+
+const KeepItWavy: NextPage = () => (
+  <SketchWrapper
+    setup={setup}
+    draw={draw}
+    mouseClicked={mouseClicked}
+    dimensions={dimensions}
+    padding={padding}
+    background={background}
+  />
+)
+
+export default KeepItWavy
---
+++ src/pages/sketches/messing-with-pi.tsx
@@ -0,0 +1,68 @@
+import SketchWrapper from 'components/SketchWrapper'
+import { NextPage } from 'next'
+import { ColorValue, Draw } from 'types/CustomP5'
+
+const width: number = 2048
+const height: number = 2048
+const dimensions: number[] = [width, height]
+const padding: number[] = [40]
+const background: ColorValue = [80, 60, 120]
+const stroke: ColorValue = [255, 250, 200]
+const numCols: number = 6
+const numRows: number = 6
+const lines: void[][] = Array.from({ length: numCols }, () =>
+  Array.from({ length: numRows })
+)
+let a: number = Math.PI
+
+const draw: Draw = p5 => {
+  const xMargin = p5.width * 0.2
+  const yMargin = p5.height * 0.2
+  const xRes = (p5.width - xMargin * 2) / numCols
+
+  p5.noLoop()
+  p5.stroke(stroke)
+  p5.fill(stroke)
+  p5.strokeWeight(p5.width * 0.0015)
+
+  lines.forEach((_, i) => {
+    _.forEach((_, j) => {
+      a = a * Math.cos(i)
+      const u = i / (numCols - 1)
+      const v = j / (numRows - 1)
+      const x = p5.lerp(xMargin, p5.width - xMargin, u)
+      const y = p5.lerp(yMargin, p5.height - yMargin, v)
+
+      p5.push()
+      p5.translate(x, y)
+      Array.from({ length: p5.TWO_PI * 5.05 }).forEach((_, i) => {
+        const a = i * 0.21
+        const r = xRes * 0.5
+        const x = r * Math.cos(a) * 0.05
+        const y = r * Math.sin(a) * 0.05
+        const speed = xRes * 0.15
+
+        p5.push()
+        p5.beginShape()
+        p5.rotate(a)
+        p5.vertex(x, y)
+        p5.translate(speed, speed)
+        p5.vertex(x + speed, y + speed)
+        p5.endShape()
+        p5.pop()
+      })
+      p5.pop()
+    })
+  })
+}
+
+const MessingWithPi: NextPage = () => (
+  <SketchWrapper
+    draw={draw}
+    dimensions={dimensions}
+    padding={padding}
+    background={background}
+  />
+)
+
+export default MessingWithPi

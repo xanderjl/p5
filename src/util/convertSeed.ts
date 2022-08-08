02416e0542bdbfdef622155a121242429f1c541d
Xander Low <alexanderjameslow@gmail.com>
2022-08-07 20:41:13 -0700

    updated conversion logic

--- src/util/convertSeed.ts
+++ src/util/convertSeed.ts
@@ -2,8 +2,8 @@ const convertSeed = (seedPhrase: string): number => {
   const bufferArray = Buffer.from(seedPhrase, 'utf-8')
   let value: number = 0
 
-  bufferArray.forEach((val, i) => {
-    value = val * 256 + bufferArray[i]
+  bufferArray.forEach((_, i) => {
+    value = value + bufferArray[i]
   })
 
   return value
